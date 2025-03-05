9506a074e5fc936fcd9c700c94ad09dbb05a3172
Raghav Mittal <111191629+royalkingzeher@users.noreply.github.com>
2025-03-05 22:32:19 +0530

    Create passportSetup.js

---
+++ config/passportSetup.js
@@ -0,0 +1,17 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+\passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, { profile, accessToken });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
